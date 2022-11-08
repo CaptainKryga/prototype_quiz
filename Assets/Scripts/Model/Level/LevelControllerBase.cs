139ddf63fbd0930cd0aac09dc70bf82e46c66f09
CaptainKryga <krygadenis@gmail.com>
2022-11-08 15:26:13 +0200

    upd base game mode

--- Assets/Scripts/Model/Level/LevelControllerBase.cs
+++ Assets/Scripts/Model/Level/LevelControllerBase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Controller.CustomInput;
 using Scriptable;
 using UnityEngine;
+using View.Game.UpdateText;
 
 namespace Model.Level
 {
@@ -8,15 +10,18 @@ namespace Model.Level
     {
         protected ModelController ModelController;
         [SerializeField] protected ScoreController ScoreController;
+        [SerializeField] protected UpdateTextUI TriesUI;
         protected GameSettings GameSettings;
+        [SerializeField] protected CustomInputBase CustomInput;
+        [SerializeField] protected WordController WordController;
         
         protected string[] Data;
-        protected int Index, IndexStart;
-        protected int Tries;
 
-        protected Action<KeyCode> ClickKey_Action;
+        //key, isClick
+        public Action<KeyCode, bool> ClickKey_Action;
 
-        public abstract void Setup(ModelController modelController, string[] data, GameSettings gameSettings);
+        public abstract void Setup(ModelController modelController, string[] data,
+            GameSettings gameSettings);
         public abstract void Restart(bool isRestart);
     }
 }
--- Assets/Scripts/Model/Level/LevelControllerEnglish.cs
+++ Assets/Scripts/Model/Level/LevelControllerEnglish.cs
@@ -1,46 +1,114 @@
-﻿using System;
-using Model.Components;
+﻿using Model.Components;
+using Model.Static;
 using Scriptable;
-using Random = UnityEngine.Random;
+using UnityEngine;
+using Utils;
 
 namespace Model.Level
 {
     public class LevelControllerEnglish : LevelControllerBase
     {
+        private void OnEnable()
+        {
+            ClickKey_Action += OnClickAnyKey;
+            CustomInput.InputKey_Action += OnClickAnyKey;
+        }
+
+        private void OnDisable()
+        {
+            ClickKey_Action -= OnClickAnyKey;
+            CustomInput.InputKey_Action -= OnClickAnyKey;
+        }
+
         private void Start()
         {
-            Cell[] cells = FindObjectsOfType<Cell>();
+            CellKeyboard[] cells = FindObjectsOfType<CellKeyboard>();
             foreach (var cell in cells)
             {
-                cell.Setup(ClickKey_Action);
+                cell.Setup(this);
             }
         }
 
-        public override void Setup(ModelController modelController, string[] data, GameSettings gameSettings)
+        public override void Setup(ModelController modelController, string[] data,
+            GameSettings gameSettings)
         {
             ModelController = modelController;
             Data = data;
             GameSettings = gameSettings;
-            IndexStart = Random.Range(0, Data.Length);
-            Index = IndexStart + 1;
-            ScoreController.Score = 0;
         }
 
         public override void Restart(bool isRestart)
         {
-            if (isRestart)
+            CellKeyboard[] cells = FindObjectsOfType<CellKeyboard>();
+            foreach (CellKeyboard cell in cells)
             {
-                ScoreController.Score = 0;
-                IndexStart = Random.Range(0, Data.Length);
-                Index = IndexStart + 1;
+                cell.Restart();
             }
             
-            StartLevel();
-        }
-
-        private void StartLevel()
-        {
+            if (isRestart)
+            {
+                GameMetrics.IndexNow = -1;//IndexEnd + 1;
+                GameMetrics.IndexEnd = Data.Length;// Random.Range(0, Data.Length);
+                ScoreController.Score = 0;
+                GameMetrics.SetNextTurn(Data, GameSettings);
+            }
             
+            if (GameMetrics.IndexNow == GameMetrics.IndexEnd || GameMetrics.IndexNow >= Data.Length)
+            {
+                ModelController.Restart(GameTypes.Menu.EndGame);
+                return;
+            }
+            
+            GameMetrics.Tries = GameSettings.DefaultTries;
+            TriesUI.SetText("Tries: " + GameMetrics.Tries);
+            GameMetrics.Word = Data[GameMetrics.IndexNow];
+            Debug.Log(GameMetrics.Word);
+            WordController.Restart(GameMetrics.Word.Length);
+        }
+        
+        private void OnClickAnyKey(KeyCode key, bool isClick)
+        {
+            if (!isClick)
+                return;
+
+            string k = key.ToString();
+            
+            if (!GameMetrics.ValidKeys.Contains(k))
+                return;
+            ClickKey_Action?.Invoke(key, false);
+
+            if (!GameMetrics.Word.Contains(k))
+            {
+                GameMetrics.Tries--;
+                TriesUI.SetText("Tries: " + GameMetrics.Tries);
+
+                if (GameMetrics.Tries <= 0)
+                {
+                    ModelController.Restart(GameTypes.Menu.GameOver);
+                    return;
+                }
+                return;
+            }
+
+            for (int x = 0; x < Data[GameMetrics.IndexNow].Length; x++)
+            {
+                if (Data[GameMetrics.IndexNow][x] == k[0])
+                {
+                    WordController.SetVisible(x, k);
+                }
+            }
+
+            GameMetrics.Word = GameMetrics.Word.Replace(k, "");
+            Debug.Log(GameMetrics.Word);
+
+            if (GameMetrics.Word.Length == 0)
+            {
+                ModelController.Restart(GameMetrics.SetNextTurn(Data, GameSettings) ?
+                    GameTypes.Menu.EndGame : GameTypes.Menu.EndLevel);
+
+                ScoreController.Score += GameMetrics.Tries;
+                return;
+            }
         }
     }
 }
